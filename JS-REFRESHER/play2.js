930561291b54e23785027b7878c97c511165561a
Luka Ernestini <luka.ernestini@gmail.com>
2022-11-07 20:54:51 +0100

    finish lect 19

--- JS-REFRESHER/play2.js
+++ JS-REFRESHER/play2.js
@@ -6,25 +6,38 @@ const person = {
   },
 };
 
-// when we store an array in a constant 'hobbies' we can still edit it,
-// because hobbies is a refference to the array. It doesn't change
-const hobbies = ["Sports", "Cooking"];
-// for (let hobby of hobbies) {
-//   console.log(hobby);
-// }
-// console.log(hobbies.map((hobby) => "Hobby: " + hobby));
-// console.log(hobbies);
-
-// hobbies.push("Programming");
-
-// spread operator (...) pulls every element out
-const coopiedArray = [...hobbies];
-console.log(coopiedArray);
-
-// rest operator (...): creates and array. Same notation, but called differently,
-// depending on what way we use it
-const toArray = (...args) => {
-  return args;
+// Object destructuring
+const printName = ({ name }) => {
+  console.log(name);
 };
 
-console.log(toArray(1, 2, 3));
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// // when we store an array in a constant 'hobbies' we can still edit it,
+// // because hobbies is a refference to the array. It doesn't change
+const hobbies = ["Sports", "Cooking"];
+const [item1, item2] = hobbies;
+console.log(item1, item2);
+// // for (let hobby of hobbies) {
+// //   console.log(hobby);
+// // }
+// // console.log(hobbies.map((hobby) => "Hobby: " + hobby));
+// // console.log(hobbies);
+
+// // hobbies.push("Programming");
+
+// // spread operator (...) pulls every element/property out
+// const coopiedArray = [...hobbies];
+// console.log(coopiedArray);
+
+// // rest operator (...): creates and array. Merges arguments into array.
+// // Same notation, but called differently,
+// // depending on what way we use it
+// const toArray = (...args) => {
+//   return args;
+// };
+
+// console.log(toArray(1, 2, 3));
