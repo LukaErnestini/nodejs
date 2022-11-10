cd04d2e4e664b1dcbea576a063b3ad183ece8b12
Luka Ernestini <luka.ernestini@gmail.com>
2022-11-10 15:07:03 +0100

    finish lect 39

---
+++ SECTION-3/routes.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("location", "/");
+        return res.end();
+      });
+    });
+  }
+  console.log("this happens, but shouldn't");
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.write("<html>");
+  //   res.write("<body>Hello!</body>");
+  //   res.write("</html>");
+  //   res.end();
+};
+
+module.exports = {
+  handler: requestHandler,
+  someText: "Some hardcoded text",
+};
