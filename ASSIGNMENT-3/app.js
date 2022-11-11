b89aed2ab82f7b3f860cc9f249a330526b189187
Luka Ernestini <luka.ernestini@gmail.com>
2022-11-11 08:44:34 +0100

    finish assignment 3

--- ASSIGNMENT-3/app.js
+++ ASSIGNMENT-3/app.js
@@ -2,16 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const userRoutes = require("./routes/users");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use("/users", userRoutes);
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
