5eee044fb552eece0a5510abe18c0f9714adaced
Luka Ernestini <luka.ernestini@gmail.com>
2022-11-12 12:09:05 +0100

    add explicit view path

--- SECTION-5/app.js
+++ SECTION-5/app.js
@@ -8,7 +8,7 @@ const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
