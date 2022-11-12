a3378250863dd6772919b08ae06f88ff50afcc16
Luka Ernestini <luka.ernestini@gmail.com>
2022-11-13 00:01:07 +0100

    switch to ejs

--- SECTION-5/app.js
+++ SECTION-5/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: path.join(__dirname, "views", "layouts"),
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
