cd6a01d8761f975cddf721ff0b6b875621a52721
Luka Ernestini <luka.ernestini@gmail.com>
2022-11-12 23:41:54 +0100

    switch to express-handlebars

--- SECTION-5/routes/shop.js
+++ SECTION-5/routes/shop.js
@@ -8,7 +8,14 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, title: "Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    title: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productsCSS: true,
+  });
 });
 
 module.exports = router;
