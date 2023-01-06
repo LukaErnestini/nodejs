014937f04dc44df22d32057962e35a22e818558e
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-06 12:33:42 +0100

    implement editing products

--- SECTION-5/controllers/products.js
+++ SECTION-5/controllers/products.js
@@ -1,9 +1,10 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     title: "Add Product",
     path: "/admin/add-product",
+    editing: "false",
   });
 };
 
@@ -23,9 +24,39 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const price = req.body.price;
   console.log(price);
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  const product = Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      title: "Edit Product",
+      path: "/admin/edit-product",
+      product: product,
+      editing: editMode,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const prodId = req.params.productId;
+  const product = new Product(prodId, title, imageUrl, description, price);
+  product.save();
+  res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
--- SECTION-5/routes/admin.js
+++ SECTION-5/routes/admin.js
@@ -8,5 +8,7 @@ router.get("/add-product", productsController.getAddProduct);
 router.get("/products", productsController.getProductsAdmin);
 
 router.post("/add-product", productsController.postAddProduct);
+router.get("/edit-product/:productId", productsController.getEditProduct);
+router.post("/edit-product/:productId", productsController.postEditProduct);
 
 module.exports = router;
