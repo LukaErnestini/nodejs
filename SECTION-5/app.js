cb1fac9a57211392aa4e605c339dd6e263377d9c
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-13 15:18:17 +0100

    create login form

--- SECTION-5/app.js
+++ SECTION-5/app.js
@@ -6,6 +6,7 @@ mongoose.set("strictQuery", true);
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 const miscController = require("./controllers/misc");
 const User = require("./models/user");
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(miscController.get404);
 mongoose
