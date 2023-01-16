f9f9bf275b35d3ec5521c577127c0c5a852901c5
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-16 19:52:19 +0100

    refactor validators

--- SECTION-5/middleware/validators.js
+++ SECTION-5/middleware/validators.js
@@ -1,4 +1,5 @@
 const { check, body } = require("express-validator");
+const User = require;
 
 // Validate the title field
 exports.title = body("title")
@@ -49,3 +50,38 @@ exports.description = body("description")
   .isLength({ max: 500 })
   .withMessage("Description must be less than 500 characters")
   .trim();
+
+exports.signupEmail = body("email")
+  .isEmail()
+  .withMessage("Invalid email.")
+  .normalizeEmail()
+  .custom((value, { req }) => {
+    // if (value === "test@test.com")
+    //   throw new Error("Example of custom validator triggered!");
+    // return true;
+    return User.findOne({ email: value }).then((userDoc) => {
+      // if this promise fulfils without error thrown, validator treats this as successful
+      // this way we added our own async validation
+      if (userDoc) {
+        return Promise.reject("That email is already registered.");
+      }
+    });
+  });
+
+exports.signupPassword = body(
+  "password",
+  "Password should be minimum 5 characters, only alphanumeric."
+)
+  .isLength({ min: 5 })
+  .isAlphanumeric()
+  .custom((pass, { req }) => {
+    if (pass !== req.body.confirmPassword) {
+      throw new Error("Passwords do not match.");
+    }
+    return true;
+  });
+
+exports.loginEmail = body("email")
+  .isEmail()
+  .withMessage("Invalid email.")
+  .normalizeEmail();
--- SECTION-5/routes/auth.js
+++ SECTION-5/routes/auth.js
@@ -1,51 +1,21 @@
 const express = require("express");
-const { check, body } = require("express-validator");
+const {
+  signupEmail,
+  signupPassword,
+  loginEmail,
+} = require("../middleware/validators");
 
 const router = express.Router();
 
 const authController = require("../controllers/auth");
-const User = require("../models/user");
 
 router.get("/login", authController.getLogin);
-router.post(
-  "/login",
-  [body("email").isEmail().withMessage("Invalid email.").normalizeEmail()],
-  authController.postLogin
-);
+router.post("/login", [loginEmail], authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.get("/signup", authController.getSignup);
 router.post(
   "/signup",
-  [
-    check("email")
-      .isEmail()
-      .withMessage("Invalid email.")
-      .normalizeEmail()
-      .custom((value, { req }) => {
-        // if (value === "test@test.com")
-        //   throw new Error("Example of custom validator triggered!");
-        // return true;
-        return User.findOne({ email: value }).then((userDoc) => {
-          // if this promise fulfils without error thrown, validator treats this as successful
-          // this way we added our own async validation
-          if (userDoc) {
-            return Promise.reject("That email is already registered.");
-          }
-        });
-      }),
-    body(
-      "password",
-      "Password should be minimum 5 characters, only alphanumeric."
-    )
-      .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body("password").custom((pass, { req }) => {
-      if (pass !== req.body.confirmPassword) {
-        throw new Error("Passwords do not match.");
-      }
-      return true;
-    }),
-  ],
+  [signupEmail, signupPassword],
   authController.postSignup
 );
 router.get("/reset", authController.getReset);
