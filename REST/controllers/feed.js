bd69801b492549d6627853238224575af38fbf9f
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-18 18:27:21 +0100

    download the frontend to test the rest api

--- REST/controllers/feed.js
+++ REST/controllers/feed.js
@@ -1,1 +1,17 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({ posts: [{ title: "post", content: "first post!" }] });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // create in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      id: 1234,
+      title,
+      content,
+    },
+  });
+};
