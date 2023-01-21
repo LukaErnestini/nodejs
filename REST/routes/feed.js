e12c0c2fc662cf26ede7eb11edfdea2a04102bd8
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-21 12:06:22 +0100

    add authorization to all post routes

--- REST/routes/feed.js
+++ REST/routes/feed.js
@@ -7,9 +7,19 @@ const isAuth = require("../middlewares/is-auth");
 const router = express.Router();
 
 router.get("/posts", isAuth, feedController.getPosts);
-router.get("/post/:postId", feedController.getPost);
-router.post("/post", [title, content, image], feedController.createPost);
-router.put("/post/:postId", [title, content], feedController.updatePost);
-router.delete("/post/:postId", feedController.removePost);
+router.get("/post/:postId", isAuth, feedController.getPost);
+router.post(
+  "/post",
+  isAuth,
+  [title, content, image],
+  feedController.createPost
+);
+router.put(
+  "/post/:postId",
+  isAuth,
+  [title, content],
+  feedController.updatePost
+);
+router.delete("/post/:postId", isAuth, feedController.removePost);
 
 module.exports = router;
