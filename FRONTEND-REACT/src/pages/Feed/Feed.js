73e8ac038e30ce21b700e8fa4f33fe2b24dc6b1a
Luka Ernestini <luka.ernestini@gmail.com>
2023-01-22 07:41:56 +0100

    test socket.io

--- FRONTEND-REACT/src/pages/Feed/Feed.js
+++ FRONTEND-REACT/src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from "react";
+import openSocket from "socket.io-client";
 
 import Post from "../../components/Feed/Post/Post";
 import Button from "../../components/Button/Button";
@@ -22,7 +23,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch("URL")
+    fetch("http://localhost:8080/auth/status", {
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+      },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch user status.");
@@ -35,7 +40,24 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    openSocket("http://localhost:8080");
   }
+
+  addPost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1,
+      };
+    });
+  };
 
   loadPosts = (direction) => {
     if (direction) {
--- REST/app.js
+++ REST/app.js
@@ -59,7 +59,16 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(mongo_uri)
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("socket.io")(server, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+      },
+    });
+    io.on("connection", (socket) => {
+      console.log("Client connected.");
+    });
   })
   .catch((err) => {
     console.log(err);
