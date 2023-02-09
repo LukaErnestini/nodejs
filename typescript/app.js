e19a13735e8ff3d13a459b1dc1e7609421890c51
Luka Ernestini <luka.ernestini@gmail.com>
2023-02-09 12:55:25 +0100

    finish lect 502

--- typescript/app.js
+++ typescript/app.js
@@ -1,13 +1,39 @@
-var num1Inp = document.getElementById("1");
-var num2Inp = document.getElementById("2");
-var btn = document.querySelector("button");
-var p = document.querySelector("p");
+"use strict";
+const num1Inp = document.getElementById("1");
+const num2Inp = document.getElementById("2");
+const btn = document.querySelector("button");
+const p = document.querySelector("p");
+const numResults = [];
+const strResults = [];
 function addNums(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + num2;
+    }
+    return +num1 + +num2;
 }
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    var n1 = num1Inp.value;
-    var n2 = num2Inp.value;
-    var res = addNums(+n1, +n2);
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
+    const n1 = num1Inp.value;
+    const n2 = num2Inp.value;
+    const res = addNums(+n1, +n2);
+    numResults.push(res);
+    const stringResult = addNums(n1, n2);
+    strResults.push(stringResult);
     console.log(res);
+    console.log(stringResult);
+    printResult({ val: res, timestamp: new Date() });
+    console.log(numResults, strResults);
+});
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("it worked!");
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log(result.split("w"));
 });
--- typescript/app.ts
+++ typescript/app.ts
@@ -3,13 +3,50 @@ const num2Inp = document.getElementById("2") as HTMLInputElement;
 const btn = document.querySelector("button");
 const p = document.querySelector("p");
 
-function addNums(num1: number, num2: number) {
-  return num1 + num2;
+const numResults: Array<number> = [];
+const strResults: string[] = [];
+
+type Numstr = number | string;
+type Result = { val: number; timestamp: Date }; //option1
+
+interface ResultObj {
+  //option2
+  val: number;
+  timestamp: Date;
+}
+
+function addNums(num1: Numstr, num2: Numstr) {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else if (typeof num1 === "string" && typeof num2 === "string") {
+    return num1 + num2;
+  }
+  return +num1 + +num2;
+}
+
+function printResult(resultObj: ResultObj) {
+  console.log(resultObj.val);
 }
 
 btn?.addEventListener("click", () => {
   const n1 = num1Inp.value;
   const n2 = num2Inp.value;
   const res = addNums(+n1, +n2);
+  numResults.push(res as number);
+  const stringResult = addNums(n1, n2);
+  strResults.push(stringResult as string);
   console.log(res);
+  console.log(stringResult);
+  printResult({ val: res as number, timestamp: new Date() });
+  console.log(numResults, strResults);
+});
+
+const myPromise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve("it worked!");
+  }, 1000);
+});
+
+myPromise.then((result) => {
+  console.log(result.split("w"));
 });
